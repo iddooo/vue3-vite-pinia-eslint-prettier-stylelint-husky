da2266d3537f9ef7dbcc5cf8398079033e76560d
jane <janedeng072@163.com>
2024-01-11 15:08:32 +0800

    feat: test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,30 +9,36 @@ const count = ref(0);
 </script>
 
 <template>
-	<h1>{{ msg }}</h1>
+	<h1>{{ msg }}{{ count }}</h1>
 
-	<div class="card">
-		<button type="button" @click="count++">count is {{ count }}</button>
-		<p>
-			Edit
-			<code>components/HelloWorld.vue</code> to test HMR
-		</p>
-	</div>
-
-	<p>
-		Check out
-		<a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite starter
-	</p>
-	<p>
-		Install
-		<a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-		in your IDE for a better DX
-	</p>
-	<p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+	<div class="abc">abc</div>
+	<ul>
+		<li>thismamclac</li>
+		<li>ncasmk</li>
+		<li>ncasmk</li>
+		<li>ncasmk</li>
+		<li>ncasmk</li>
+		<li>ncasmk</li>
+		<li>ncasmk</li>
+		<li>ncasmk</li>
+	</ul>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .read-the-docs {
 	color: #888;
+}
+
+.abc {
+	font-size: 20px;
+}
+
+ul > li {
+	font-size: 12px;
+	color: red;
+}
+
+ul > li:nth-of-type(even) {
+	background-color: rgb(247 247 8);
 }
 </style>
